301636c4126e64f14ef59bc1de2d5fdd63c04da1
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2023-10-10 10:49:57 +0530

    Update virtualmachine.tf

--- assessment1-three-tier-architecture/virtualmachine.tf
+++ assessment1-three-tier-architecture/virtualmachine.tf
@@ -1,14 +1,14 @@
 resource "azurerm_virtual_network" "webservervnet" {
   name                = "webserver-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
 }
 
 resource "azurerm_subnet" "webserver" {
   name                 = "testsubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.testrg.name
+  virtual_network_name = azurerm_virtual_network.testrg.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
@@ -16,10 +16,10 @@ data "azurerm_key_vault_secret" "credentials" {
   name         = "password"
   key_vault_id = "/subscriptions/xxxxx/resourceGroups/yyyyy/providers/Microsoft.KeyVault/vaults/zzzzz"
 }
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "test_nic" {
   name                = "test-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
 
   ip_configuration {
     name                          = "testip"
@@ -30,14 +30,14 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_windows_virtual_machine" "webservervm" {
   name                = "test-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.testrg.name
+  location            = azurerm_resource_group.testrg.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = data.azurerm_key_vault_secret.credentials.value
   availability_set_id = azurerm_availability_set.web_availabilty_set.id
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.test_nic.id,
   ]
 
   os_disk {
