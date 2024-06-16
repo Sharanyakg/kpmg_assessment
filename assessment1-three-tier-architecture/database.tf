665d81ab2c252ab3faf2d6795a630f7b13a9f445
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-16 17:11:32 +0530

    Create database.tf

---
+++ assessment1-three-tier-architecture/database.tf
@@ -0,0 +1,16 @@
+resource "azurerm_sql_server" "example" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = var.sql_database_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  sku_name            = "S1"
+}
