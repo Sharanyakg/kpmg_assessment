5f6561eff6d99c7fb1e6ad1e808e33d17bf628be
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-16 17:02:28 +0530

    Delete assessment1-three-tier-architecture/availabilityset.tf

--- assessment1-three-tier-architecture/availabilityset.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_availability_set" "webserver" {
-  name                = var.availability_set_name
-  location            = azurerm_resource_group.testrg.location
-  resource_group_name = azurerm_resource_group.testrg.name
-}
