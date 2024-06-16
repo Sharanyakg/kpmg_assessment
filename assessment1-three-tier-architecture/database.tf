3c43610d2156755a002e42397566413f5aeb1771
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-16 17:19:00 +0530

    Update database.tf

--- assessment1-three-tier-architecture/database.tf
+++ assessment1-three-tier-architecture/database.tf
@@ -1,16 +1,16 @@
-resource "azurerm_sql_server" "example" {
+resource "azurerm_sql_server" "assessment" {
   name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.assessment.name
+  location                     = azurerm_resource_group.assessment.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_sql_database" "example" {
+resource "azurerm_sql_database" "assessment" {
   name                = var.sql_database_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
+  resource_group_name = azurerm_resource_group.assessment.name
+  location            = azurerm_resource_group.assessment.location
+  server_name         = azurerm_sql_server.assessment.name
   sku_name            = "S1"
 }
