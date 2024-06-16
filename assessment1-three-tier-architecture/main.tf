05853e2428da10103b86356f88d6d263bcae0c9a
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-16 17:17:33 +0530

    Update main.tf

--- assessment1-three-tier-architecture/main.tf
+++ assessment1-three-tier-architecture/main.tf
@@ -12,17 +12,17 @@ terraform {
 }
 
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "assessment" {
   name     = var.resource_group_name
   location = var.location
 }
 
-data "azurerm_key_vault" "example" {
+data "azurerm_key_vault" "assessment" {
   name                = var.key_vault_name
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.assessment.name
 }
 
 data "azurerm_key_vault_secret" "sql_connection_string" {
   name         = var.sql_connection_string_secret_name
-  key_vault_id = data.azurerm_key_vault.example.id
+  key_vault_id = data.azurerm_key_vault.assessment.id
 }
