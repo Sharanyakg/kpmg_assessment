b1ce7c667d03bc928898e785861c6442f1d1ff98
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-17 16:52:33 +0530

    Update database.tf

--- assessment1-three-tier-architecture/database.tf
+++ assessment1-three-tier-architecture/database.tf
@@ -3,7 +3,7 @@ resource "azurerm_sql_server" "assessment" {
   resource_group_name          = azurerm_resource_group.assessment.name
   location                     = azurerm_resource_group.assessment.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_username
+  administrator_login          = data.azurerm_key_vault_secret.sql_admin_username.value
   administrator_login_password = var.sql_admin_password
 }
 
