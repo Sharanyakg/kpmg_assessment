af9701bac32f1267d1a8ffb826f01e22e92e589c
Sharanyakg <37186842+Sharanyakg@users.noreply.github.com>
2024-06-17 16:51:19 +0530

    Update main.tf

--- assessment1-three-tier-architecture/main.tf
+++ assessment1-three-tier-architecture/main.tf
@@ -22,6 +22,16 @@ data "azurerm_key_vault" "assessment" {
   resource_group_name = azurerm_resource_group.assessment.name
 }
 
+data "azurerm_key_vault_secret" "sql_admin_username" {
+  name         = var.sql_admin_username_secret_name
+  key_vault_id = data.azurerm_key_vault.assessment.id
+}
+
+data "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = var.sql_admin_password_secret_name
+  key_vault_id = data.azurerm_key_vault.assessment.id
+}
+
 data "azurerm_key_vault_secret" "sql_connection_string" {
   name         = var.sql_connection_string_secret_name
   key_vault_id = data.azurerm_key_vault.assessment.id
